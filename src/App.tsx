3abe82f39d44639d684b6803f16ec044b3d36ccb
andriuszaz <a.zazeckas@gmail.com>
2023-11-03 12:04:13 +0200

    refactor: App and Header components refactored to functional components

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,35 @@
-import { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
-import Header from './Components/Header/Header';
+import { Header } from './Components/Header/Header';
 import { Search } from './Components/Search/Search';
 import { Results } from './Components/Results/Results';
+
 interface AppProps {}
-interface AppState {
-  searchValue: string;
-  error: boolean;
-}
 
-export default class App extends Component<AppProps, AppState> {
-  constructor(props: AppProps) {
-    super(props);
-    const savedSearchValue = localStorage.getItem('searchValue');
-    this.state = {
-      searchValue: savedSearchValue || '',
-      error: false,
-    };
-  }
-  handleSearchChange = (value: string) => {
-    this.setState({ searchValue: value });
+export const App: React.FC<AppProps> = () => {
+  const [searchValue, setSearchValue] = useState(
+    localStorage.getItem('searchValue') || ''
+  );
+  const [error, setError] = useState(false);
+
+  const handleSearchChange = (value: string) => {
+    setSearchValue(value);
   };
 
-  throwNewError = () => {
-    this.setState({
-      error: true,
-    });
+  const throwNewError = () => {
+    setError(true);
   };
-  render() {
-    if (this.state.error) {
-      throw new Error('Lets test the error boundary');
-    }
 
-    const { searchValue } = this.state;
-    return (
-      <div className="App">
-        <Header />
-        <button onClick={this.throwNewError}>Error test button</button>
-        <Search
-          onSearchChange={this.handleSearchChange}
-          searchValue={searchValue}
-        />
-        <Results
-          searchValue={searchValue}
-          onSearchChange={this.handleSearchChange}
-        />
-      </div>
-    );
+  if (error) {
+    throw new Error('Lets test the error boundary');
   }
-}
+
+  return (
+    <div className="App">
+      <Header />
+      <button onClick={throwNewError}>Error test button</button>
+      <Search onSearchChange={handleSearchChange} searchValue={searchValue} />
+      <Results searchValue={searchValue} onSearchChange={handleSearchChange} />
+    </div>
+  );
+};
--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,11 +1,7 @@
-import { Component } from 'react';
-
-export default class Header extends Component {
-  render() {
-    return (
-      <header>
-        <h1>Welcome to the world of Star Wars</h1>
-      </header>
-    );
-  }
-}
+export const Header = () => {
+  return (
+    <header>
+      <h1>Welcome to the world of Star Wars</h1>
+    </header>
+  );
+};
